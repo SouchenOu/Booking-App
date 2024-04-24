d292f26c01b9dc1f9bc5732859e1d5ba60d59e38
SouchenOu <soukainaouchenuai@gmail.com>
2024-04-24 16:10:58 -0400

    adding update the hotel request we just write the id of the hotel that we want to change in the params and then write in the body what you want to change in

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -13,13 +13,32 @@ router.post("/", async (req, res)=>{
     try{
 
         const savedHotel = await newHotel.save();
-        res.status(200).json();
+        res.status(200).json(savedHotel);
 
     }catch(err){
         //JSON is often used when data is sent from a server to a web page
         res.status(500).json(err);
 
     }
+})
+
+/********Update the hotel */
+
+router.put('/:id', async (req, res)=>{
+
+    try{
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set : req.body});
+        //we do this one if we want to see the new result we add -->({new : true})
+        // const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set : req.body}, {new: true});
+
+        res.status(200).json(updateHotel);
+
+
+    }catch(err){
+        res.status.json(err);
+
+    }
+
 })
 
 export default router;
